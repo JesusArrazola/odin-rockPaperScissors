a95861fbec99d15b83db9a1ed355f4e797c6a6b5
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-21 11:36:30 -0600

    compare function done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 function computerPlay(){
-    let options = ['Rock','Paper','Scissors'];
+    let options = ['rock','paper','scissors'];
     return options[Math.floor( Math.random()*3)];
+}
+
+function compare(a,b){
+    if(a == b) return -1; //Draw
+
+    if(a === "rock"){
+        if(b === "paper") return 0;
+        if(b === "scissors") return 1;
+
+    }else if(a == "paper"){
+
+        if(b === "rock") return 1;
+        if(b === "scissors") return 0;
+
+    }else{
+        if(b === "rock") return 0;
+        if(b === "paper") return 1;
+    }
+}
+
+function singleRound(playerSelection,computerSelection){
+    let roundResult = compare(playerSelection.toLowerCase(),computerPlay());
+
 }
