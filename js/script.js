34258c1bd4a2f48ef59b9c3f70da6686dd86e594
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-21 12:07:51 -0600

    Game finished

--- js/script.js
+++ js/script.js
@@ -21,10 +21,32 @@ function compare(a,b){
     }
 }
 
-function singleRound(playerSelection,computerSelection){
-    let roundResult = compare(playerSelection.toLowerCase(),computerSelection);
-
+function printResult(playerSelection,computerSelection,roundResult){
     if(roundResult === -1) return `Draw! both selected ${playerSelection}.`;
-
     return `${(roundResult===1)?`You win! ${playerSelection} beats ${computerSelection}`:`You lose! ${computerSelection} beats ${playerSelection}` }`;
+}
+
+function game(){
+    let userScore = 0;
+    let computerScore = 0;
+    let roundResult;
+    let player = "";
+    let computer = "";
+    let options = ['rock','paper','scissors'];
+
+    for(let i=0;i<5;i++){
+        while(!options.includes(player.toLowerCase())){
+            player = prompt("Type your choice");
+        }
+        computer = computerPlay();
+        roundResult = compare(player,computer);
+        if(roundResult === 1) userScore++;
+        else if (roundResult === 0) computerScore++;
+        console.log(printResult(player,computer,roundResult));
+        player = "";
+        computer = "";
+    }
+    if(userScore > computerScore) console.log(`You win! ${userScore} out of 5`);
+    else if(userScore < computerScore) console.log(`You lose! ${userScore} out of 5`);
+    else console.log(`Draw! Both scored ${userScore}; Drawed ${5-userScore} times`);
 }
